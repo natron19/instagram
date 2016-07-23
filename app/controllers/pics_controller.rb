8e89a9214208afa857e2bfb88c46ab32f6653a7e
natron19 <nathan19@hotmail.com>
2016-07-22 17:43:19 -0700

    adds edit and destroy actions

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -2,7 +2,11 @@ class PicsController < ApplicationController
   before_action :find_pic, only: [:show, :edit, :update, :destroy]
 
   def index
-    @pics = Pics.all
+    @pics = Pic.all.order("created_at DESC")
+  end
+
+
+  def show
   end
 
   def new
@@ -19,9 +23,22 @@ class PicsController < ApplicationController
     end
   end
 
-  def show
-
+  def edit
   end
+
+  def update
+    if @pic.update(pic_params)
+      redirect_to @pic, notice: "Pic was updated!"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @pic.destroy
+    redirect_to root_path
+  end
+
 
   private
 

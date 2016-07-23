904f9104cf62f6f4e89f03c8293a6b97b75528c1
natron19 <nathan19@hotmail.com>
2016-07-22 17:24:09 -0700

    adds pic actions and views for new

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,5 +1,20 @@
 class PicsController < ApplicationController
   def index
+    @pics = Pics.all
+  end
+
+  def new
+    @pic = Pic.new
+  end
+
+  def create
+    @pic = Pic.new(pic_params)
+  end
+
+  private
+
+  def pic_params
+    params.require(:pic).permit(:title, :description)
   end
 
 
